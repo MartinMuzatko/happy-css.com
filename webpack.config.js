0590ef0a12771a4353d8acac6b4e0d022e2f979c
Martin Muzatko <martin@happy-css.com>
2016-12-20 14:36:38 +0100

    Push project settings

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var webpack = require('webpack')
+var postcss = require('postcss')
+var autoprefixer = require('autoprefixer')
+var cssnano = require('cssnano')
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: './',
+        filename: '[name].js',
+        sourceMapFilename: "[name].js.map",
+    },
+    module: {
+        preLoaders: [
+            { test: /\.html$/, loader: 'riotjs' },
+            { test: /\.js$/, loader: 'source-map' },
+        ],
+        loaders: [
+            { test: /\.(jpe?g|png|gif|mp4)$/i, loader: 'file?name=img/[name].[ext]'},
+            { test: /fonts\/.*\.(woff|eot|svg|otf|ttf)$/i, loader: `file?name=fonts/[name].[ext]`},
+            { test: /\.html$|\.js$/, loader: 'babel', query: { presets: 'es2015-riot' }},
+            { test: /\.less$/, loader: ExtractTextPlugin.extract('raw!postcss!less')},
+            { test: /\.json$/, loader: 'json'},
+        ]
+    },
+    postcss: () => {
+        return [
+            autoprefixer({browsers: 'last 2 versions'}),
+            cssnano(),
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            riot: 'riot'
+        }),
+        new ExtractTextPlugin("[name].css")
+    ],
+    devServer: {
+        port: 8080,
+        inline: true
+    },
+    eslint: {
+        configFile: './.eslintrc'
+    },
+    devtool: 'source-map'
+}
