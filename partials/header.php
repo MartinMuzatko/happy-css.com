cc64337fb399c17f42eb807af293fa909a8ef57e
Martin Muzatko <martin@happy-css.com>
2017-05-17 09:37:06 +0200

    Color navigation items on subpages and add content template name to stripes

--- partials/header.php
+++ partials/header.php
@@ -1,12 +1,17 @@
 <?php namespace ProcessWire; ?>
 <header class="stripe fluid header reduced">
-    <div layout="row" layout-align="start center">
+    <div layout="row" layout-align="start stretch">
         <a href="<?=$homepage->httpUrl?>">
             <img src="<?=$homepage->image->get('name%=light')->size(180,180)->httpUrl?>" alt="" height="46" width="46">
         </a>
-        <nav>
+        <nav layout="row">
             <? foreach ($homepage->children('template=page|overview') as $key => $child):?>
-                <a href="<?=$child->url?>"><?=$child->title?></a>
+                <style>
+                    #nav-<?=$child->name?>:hover, #nav-<?=$child->name?>:focus, #nav-<?=$child->name?>.active {
+                        background-color: <?=$child->color?>
+                    }
+                </style>
+                <a class="<?= $child->id == $page->rootParent->id ? 'active' : ''?>" id="nav-<?=$child->name?>" href="<?=$child->url?>"><?=$child->title?></a>
             <? endforeach; ?>
         </nav>
         <form action="<?=$pages->get('/search/')->httpUrl?>" class="search" flex-end>
