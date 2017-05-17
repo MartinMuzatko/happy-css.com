c217efb1d1f69ab022d3c4990fd042d697023ff4
Martin Muzatko <martin@happy-css.com>
2017-05-17 09:38:33 +0200

    Removed rellax from deps again

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import './flex.scss'
 import './js/vendor/prism.js'
 import riot from 'riot/riot+compiler'
 window.riot = riot
-import Rellax from 'rellax'
-new Rellax('header');
+// import Rellax from 'rellax'
+// new Rellax('header');
