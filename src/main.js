aedb04eb66dbe9a623501dbcfff884093f9a297a
Martin Muzatko <martin@happy-css.com>
2017-05-31 09:19:57 +0200

    Split vendor styles and main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import './less/main.less'
-import './flex.scss'
 //import './js/tags/add-content.html'
 //import './js/vendor/caniuse.min.js'
 // import Rellax from 'rellax'
--- src/vendor.js
+++ src/vendor.js
@@ -1,3 +1,5 @@
+import './less/vendor.less'
+import './flex.scss'
 import './js/vendor/prism.js'
 import riot from 'riot/riot+compiler'
 window.riot = riot
