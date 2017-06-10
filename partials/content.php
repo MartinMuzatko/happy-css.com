68fc396edf2a38da8a7a217f140b7222ae833da3
Martin Muzatko <martin@happy-css.com>
2017-06-10 14:22:09 +0200

    Fix IE10 flex layout bug

--- partials/content.php
+++ partials/content.php
@@ -7,26 +7,28 @@
     ?>
     <section
         id="<?= strtolower(str_replace(' ', '', $content->title))?>"
-        class="stripe stripe--<?=$content->backgroundcolor->title?> <?=$content->stripewidth->title?> <?=$content->template->name?>">
+        class="stripe stripe--<?=$content->backgroundcolor->title?> stripe--<?=$content->stripewidth->title?> <?=$content->template->name?>">
 
         <?=editButton($content)?>
         <div>
             <? if(!$content->hidetitle):?>
                 <h2><?=$content->title?></h2>
             <? endif ?>
-            <div layout="row" layout-align="space-between">
-                <? foreach($content->textgrid as $text): ?>
-                    <?php
-                        $flex = 'flex="100"';
-                        if (count($content->textgrid) >= 2) {
-                            $flex = 'flex="100" flex-gt-md="45"';
-                        }
-                    ?>
-                    <div <?=$flex?>>
-                        <?=$text->text?>
-                    </div>
-                <? endforeach; ?>
-            </div>
+            <? if($content->textgrid):?>
+                <div layout="row" layout-align="space-between">
+                    <? foreach($content->textgrid as $text): ?>
+                        <?php
+                            $flex = 'flex="100"';
+                            if (count($content->textgrid) >= 2) {
+                                $flex = 'flex="100" flex-gt-md="45"';
+                            }
+                        ?>
+                        <div <?=$flex?>>
+                            <?=$text->text?>
+                        </div>
+                    <? endforeach; ?>
+                </div>
+            <? endif ?>
             <?php
 
                 if ($content->template->name != 'content-text') {
--- partials/header.php
+++ partials/header.php
@@ -1,10 +1,10 @@
 <?php namespace ProcessWire; ?>
 <header class="stripe stripe--fluid header header--reduced">
-    <div layout="row" layout-align="start stretch">
+    <div layout="column" layout-gt-sm="row" layout-align="start stretch">
         <a class="site__logo site__logo--reduced" href="<?=$homepage->httpUrl?>">
             <img srcset="<?=$homepage->image->get('name%=light')->size(92,92)->httpUrl?> 2x" src="<?=$homepage->image->get('name%=light')->size(180,180)->httpUrl?>" alt="" height="46" width="46">
         </a>
-        <nav class="site__nav site__nav--short" layout="row">
+        <nav class="site__nav site__nav--short" layout-gt-sm="row" layout="column">
             <? foreach ($homepage->children('template=page|overview') as $key => $child):?>
                 <style>
                     #nav-<?=$child->name?>:hover, #nav-<?=$child->name?>:focus, #nav-<?=$child->name?>.active {
