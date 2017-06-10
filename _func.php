ea9d36c0f7473b406c1436a6802701a250ee8119
Martin Muzatko <martin@happy-css.com>
2017-06-10 14:22:26 +0200

    Make edit button more visible

--- _func.php
+++ _func.php
@@ -4,7 +4,7 @@ function editButton($page) {
 	ob_start();
 	?>
 	<? if(wire('user')->isLoggedin()): ?>
-        <p style="position: absolute; background: rgba(255,255,255,.25); padding: .5em; top: 0; right: 0; z-index: 100000; width: auto;">
+        <p style="position: absolute; background: rgba(255,255,255,.5); padding: .5em; top: 0; right: 0; z-index: 100000; width: auto;">
             <a style="color: black;" href="<?=$page->editURL?>">Edit</a>
         </p>
     <? endif;
