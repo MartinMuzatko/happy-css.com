de7ef869e59fa2469e78fa1caa772c12e9116991
Martin Muzatko <martin@happy-css.com>
2017-06-27 08:32:10 +0200

    Removed unneeded dependencies

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import './less/main.less'
 import './js/tags/code-preview.html'
-//import './js/tags/add-content.html'
-//import './js/vendor/caniuse.min.js'
-// import Rellax from 'rellax'
-// new Rellax('header');
 
 if (riot) {
     riot.mount('*')
