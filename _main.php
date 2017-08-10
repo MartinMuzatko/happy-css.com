363936d03a2026da52bf13e86a9c9daf4f66d128
Martin Muzatko <martin@happy-css.com>
2017-08-10 14:13:26 +0200

    Fix isLoggedin for analytics

--- _main.php
+++ _main.php
@@ -80,7 +80,7 @@ $htmlTitle = $page->title == $homepage->title ? $page->title : "$page->title | $
             </p>
         </div>
     </footer>
-    <? if(!$user->isLoggedin): ?>
+    <? if(!$user->isLoggedin()): ?>
         <script>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
