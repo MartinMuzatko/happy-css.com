f2c64c74d7e9ccda1f218f3c96f0a5e4fb62448c
Martin Muzatko <martin@happy-css.com>
2017-09-19 10:14:50 +0200

    Make dates appear less prominent and sort articles by published date

--- tag.php
+++ tag.php
@@ -1,24 +1,28 @@
 <?php namespace ProcessWire; ?>
 <?php include('partials/header.php');?>
 <main class="site__content">
-    <header
+    <div
         class="stripe stripe--primary">
         <?=editButton($page)?>
         <div>
             <div class="title">
                 <?php
-                $articles = $pages->find('tags='.$page->title);
+                $articles = $pages->find('tags='.$page->title.',sort=-published');
                 $count = $articles->count;
                 ?>
                 <h1><?=$page->title?></h1>
                 <p class="description">
 					<strong><?=$count?> article<?=$count>1?'s':''?></strong> tagged with <span class="tag" style="background:<?=$page->color?>">#<?=$page->title?></span>
 				</p>
-				<?=$page->text?>
             </div>
         </div>
-    </header>
+    </div>
     <section>
+		<div>
+			<?=$page->text?>
+		</div>
+	</section>
+	<section>
     	<? foreach($articles as $article): ?>
     		<?=articlePreview($article)?>
     	<? endforeach; ?>
